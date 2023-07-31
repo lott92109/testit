2fa08c92f682f625bab7800f0f2f7b0cbac495ed
lott92109 <140354087+lott92109@users.noreply.github.com>
2023-07-31 01:38:09 +0000

    typos corrections

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import random
 x = int(
     input(
-        "Welcome to the guessing game. Please guess a whole number from 1 and 10."
+        "Welcome to the guessing game. Please guess a whole number from 1 and 10 including 1 and 10."
     ))
 y = int(random.randint(1, 10))
 if x == y:
   print("Congratulation. You guessed correctly and won.")
 elif x<1:
-  print("You number you enter was smaller then 1. Pleas run the game again")
+  print("The number you enter was smaller then 1. Pleas run the game again.")
 elif x >10:
-  print ("Your number you enter was greater than 10. Pleas run the game again")
+  print ("The number you enter was greater than 10. Pleas run the game again.")
 else:
   print("Sorry your guess was wrong. The number was", y)
